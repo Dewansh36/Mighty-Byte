aad96c77f7aee3c74ce845643369dcae902d4052
parthiv360 <parthivsarkar360@gmail.com>
2022-06-18 13:17:17 +0530

    selectpage+footer

--- frontend/src/Components/Select/Select.js
+++ frontend/src/Components/Select/Select.js
@@ -15,12 +15,18 @@ import image from '../Select/img2.gif'
 function Select() {
     const[flip,setFlip]= useState(false);
     const[flip1,setFlip1]=useState(false);
+    const[flip2,setFlip2]=useState(false);
+
     const cardflip=()=>{
         setFlip(flip=>!flip)
 
     }
     const cardflip1=()=>{
         setFlip1(flip1=>!flip1)
+    }
+    const cardflip2=()=>{
+        setFlip2(flip2=>!flip2)
+
     }
     const [loading, setLoading]=useState(true);
     const [curUser, setUser]=useGetUser({});
@@ -42,45 +48,17 @@ function Select() {
         )
     }
     return (
-        <div>
+        <div >
             <Navbar user={curUser} />
             <ToastContainer position='top-center' />
-            {/* <div className={Styles.main}>
-                <Container>
-                    <Row>
-                        <Col lg={6} md={12} className={Styles.separate}>
-                            <h1 className={`${Styles.fix} ${Styles.SelectH1}`}>Today is a great day to CODE.</h1>
-                            <div className={`${Styles.maincontainer} ${Styles.card1}`}>
-                                <div className={Styles.thecard}>
-                                    <div className={Styles.thefront}><div className={`${Styles.cardH1} ${Styles.SelectH1}`}>For Developers</div>
-                                    </div>
-                                    <div className={Styles.theback}><p className={Styles.SelectP}>Upload projects and check out others projects, make friends working in same domain.</p>
-                                        <span className={Styles.SelectButton}><a href="#"></a></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={`${Styles.maincontainer} ${Styles.card2}`}>
-                                <div className={Styles.thecard}>
-                                    <div className={Styles.thefront}><div className={`${Styles.cardH1} ${Styles.SelectH1}`}>CP Enthusiasts</div>
-                                    </div>
-                                    <div className={Styles.theback}><p className={Styles.SelectP}>Get all the data about CP and get recommended problems based on your rating .</p>
-                                        <span className={Styles.SelectButton}><a href="#"></a></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col lg={6} md={12}>
-                            <img src={SelectPageImage} alt="iphone-mockup" className={Styles["title-img"]} />
-                        </Col>
-                    </Row>
-                </Container>
-            </div> */}
-            <div >
+           
+            <div className='s-title-head'>
                 <h1 className='s-heading'>Today is a great day to ....Explore</h1>
             </div>
-            <div className='s-c1'>
+            <div className='container'>
+            <div className='s-c1 row justify-content-around align-items-center' >
 
-                <div className={flip ? 's-element-card open':'s-element-card'} onClick={cardflip}>
+                <div className={flip ? 's-element-card open col-2':'s-element-card col-2'} onClick={cardflip}>
                     <div className="front-facing">
                         <h1 className="abr">CP</h1>
                         <p className="title">Enthusiast</p>
@@ -89,10 +67,10 @@ function Select() {
                     </div>
                     <div className="back-facing">
                         <p className='s-text'>Get all the data about CP and get recommended problems based on your rating .</p>
-                        <p><a className="btn" href="" target="_blank">Explore</a></p>
+                        <p><a className="btn" href="/cp" >Explore</a></p>
                     </div>
                 </div>
-                <div className={flip1 ? 's-element-card open':'s-element-card'} onClick={cardflip1}>
+                <div className={flip1 ? 's-element-card open col-2':'s-element-card col-2'} onClick={cardflip1}>
                     <div className="front-facing">
                         <h1 className="abr">For</h1>
                         <p className="title">Developers</p>
@@ -101,13 +79,28 @@ function Select() {
                     </div>
                     <div className="back-facing">
                         <p className='s-text'>Upload projects and check out others projects, make friends working in same domain.</p>
-                        <p><a className="btn" href="" target="_blank">Explore</a></p>
+                        <p><a className="btn" href="/posts" >Explore</a></p>
                     </div>
                 </div>
+                
+                <div className={flip2 ? 's-element-card open col-2':'s-element-card col-2'} onClick={cardflip2}>
+                    <div className="front-facing">
+                        <h1 className="abr">Dev</h1>
+                        <p className="title">Flash</p>
+                        <span className="upper">Google</span>
+                        <span className="below">Apple</span>
+                    </div>
+                    <div className="back-facing">
+                        <p className='s-text'>Whether it is the invention of new Gadget or updation of old , you can know everything.</p>
+                        <p><a className="btn" href="/news" >Explore</a></p>
+                    </div>
+                </div>
+                <div className='s-image col-2'>
+                    <img src={image} alt=""  width={"400px"}/>
+                </div>
+                </div>
             </div>
-            <div className='s-image'>
-                <img src={image} alt="" />
-            </div>
+            
         </div>
     )
 }
