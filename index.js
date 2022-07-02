f079072732e94fb3a35c7caa1084278eb3d0fd64
Dewansh Chandra <dewansh.chandra@gmail.com>
2022-07-02 21:29:51 +0530

    Back-to-normal

--- index.js
+++ index.js
@@ -29,15 +29,6 @@ app.use(
     })
 );
 
-if (process.env.NODE_ENV==="production") {
-    //Set static folder
-    app.use(express.static("frontend/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    });
-}
-
-
 //Setting Up mongoose
 const connectDatabase=() => {
     // console.log('Connecting!');
