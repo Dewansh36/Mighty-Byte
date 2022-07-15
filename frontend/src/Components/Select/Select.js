9cedbc60cd24136433a20822095d602c16b74831
Hriday Garg <hriday.1508.garg@gmail.com>
2022-07-15 17:28:36 +0530

    select page done

--- frontend/src/Components/Select/Select.js
+++ frontend/src/Components/Select/Select.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
-// import { Container, Row, Col } from 'react-bootstrap'
 import '../../Public/css/SelectPage.css'
 import SelectPageImage from '../../Public/image/SelectPageImage.png'
 import Loading from '../loading';
@@ -72,9 +70,9 @@ function Select() {
                 <h1 className='s-heading'>Today is a great day to ....Explore</h1>
             </div>
             <div className='container'>
-                <div className='s-c1 row justify-content-around align-items-center' >
+                <div className='s-c1 row' >
 
-                    <div className={flip? 's-element-card open col-2':'s-element-card col-2'} onClick={cardflip}>
+                    <div className={flip? 's-element-card open col-2 scard1':'s-element-card col-2 scard1'} onClick={cardflip}>
                         <div className="front-facing">
                             <h1 className="abr">CP</h1>
                             <p className="title">Enthusiast</p>
@@ -86,7 +84,7 @@ function Select() {
                             <p><a className="btn" href="/cp" ><GodBtn text="Explore" height="2rem" width="1rem"></GodBtn></a></p>
                         </div>
                     </div>
-                    <div className={flip1? 's-element-card open col-2':'s-element-card col-2'} onClick={cardflip1}>
+                    <div className={flip1? 's-element-card open col-2 scard2':'s-element-card col-2 scard2'} onClick={cardflip1}>
                         <div className="front-facing">
                             <h1 className="abr">For</h1>
                             <p className="title">Developers</p>
@@ -99,7 +97,7 @@ function Select() {
                         </div>
                     </div>
 
-                    <div className={flip2? 's-element-card open col-2':'s-element-card col-2'} onClick={cardflip2}>
+                    <div className={flip2? 's-element-card open col-2 scard3':'s-element-card col-2 scard3'} onClick={cardflip2}>
                         <div className="front-facing">
                             <h1 className="abr">Dev</h1>
                             <p className="title">Flash</p>
@@ -111,15 +109,9 @@ function Select() {
                             <p><a href="/news" ><GodBtn text="Explore" pd="3rem"></GodBtn></a></p>
                         </div>
                     </div>
-                    {/* <div className="back-facing">
-                        <p className='s-text'>Get all the data about CP and get recommended problems based on your rating .</p>
-                        <p><a href="/cp" ><GodBtn text="Explore" pd="3rem"></GodBtn></a></p>
-                    </div> */}
                 </div>
-
             </div>
-
-        </div>
+            </div>
     )
 }
 
